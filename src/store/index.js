2feec69fe239250b1eaecd636e475993ffb7e659
thetpainghtut <thetpainghtut.bf@gmail.com>
2022-01-19 20:02:09 +0630

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,29 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
+const axios = require('axios');
 
 export default new Vuex.Store({
   state: {
+    loadingStatus: true,
+    countries: []
   },
   mutations: {
+    SET_LOADING_STATUS(state,status){
+      state.loadingStatus = status
+    },
+    SET_COUNTRIES(state, countries){
+      state.countries = countries
+    }
   },
   actions: {
+    getData(context){
+      axios.get('https://restcountries.com/v3.1/all')
+          .then(response => {
+            context.commit('SET_LOADING_STATUS',false)
+            context.commit('SET_COUNTRIES',response.data)
+          })
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="home">
+    <h2 v-if="this.$store.state.loadingStatus">No Read</h2>
+    <h2 v-else>Get All Data</h2>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -13,6 +15,11 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data(){
+    return {
+      
+    }
   }
 }
 </script>
